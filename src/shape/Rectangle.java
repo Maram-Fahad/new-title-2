e176f097b09d5e9feaeb8fc54d4c3158edd310ac
maram fahad <fmaram.2001@gmail.com>
2022-10-22 22:37:21 +0300

    adding write code to calculate perimeter

--- src/shape/Rectangle.java
+++ src/shape/Rectangle.java
@@ -18,7 +18,9 @@ public class Rectangle {
         return length*breadth; // write code to calculate area here
     } 
     
-    // write code to calculate perimeter here
+    public double getPerimeter(){
+        return (2*length)+(2*breadth);
+    } // write code to calculate perimeter here
     // getPerimeter()
     
 }
